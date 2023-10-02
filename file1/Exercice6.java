ba821f568e02e848bd076d012765af27637349f0
GuilhermeDias02 <guilherme.alex.dias@gmail.com>
2023-10-02 10:56:46 +0200

    ex 6

---
+++ file1/Exercice6.java
@@ -0,0 +1,12 @@
+public class Exercice6{
+    public static void main(String[] args){
+        String str = "abcdefghijklmnopqrstuvwxyz";
+        Integer i = str.length();
+
+        while(i > 0){
+            System.out.print(str.charAt(i-1));
+
+            i--;
+        }
+	}
+}
