a35692b8e4a8104d0fdc52adf20ae87b7293ebb5
GuilhermeDias02 <guilherme.alex.dias@gmail.com>
2023-10-02 11:05:26 +0200

    ex 7

---
+++ file1/Exercice7.java
@@ -0,0 +1,11 @@
+public class Exercice7{
+    public static void main(String[] args){
+        int nbr = 5;
+
+        for(int i = 0; i <=10; i++){
+            int res = i * nbr;
+
+            System.out.println(i + " x " + nbr + " = " + res);
+        }
+	}
+}
