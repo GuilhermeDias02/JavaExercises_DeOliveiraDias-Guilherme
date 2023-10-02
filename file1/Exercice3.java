8a90be0357aae2f11a95b96973a0968ca1848265
GuilhermeDias02 <guilherme.alex.dias@gmail.com>
2023-10-02 10:12:57 +0200

    ex 3'

---
+++ file1/Exercice3.java
@@ -0,0 +1,9 @@
+public class Exercice3{
+    public static void main(String[] args){
+        String name = "Halil";
+        Integer age = 27;
+        String job = "developpeur";
+
+        System.out.println("je m'appelle " + name + ", j'ai " + age + " ans et je suis " + job);
+	}
+}
